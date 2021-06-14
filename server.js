43f4e002f9627f910272acf634bbdd512bbf9ca7
salammustafa728 <salammustafa728@gmail.com>
2021-06-14 20:15:20 +0300

    test

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express() // initialize your express app instance
 app.use(cors()) // after you initialize your express app instance
 // a server endpoint 
 
-const port=process.env.port;
+const PORT=process.env.PORT;
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
   res.send('Hello World') // our endpoint function response
 })
  
-app.listen(port)
+app.listen(PORT)
