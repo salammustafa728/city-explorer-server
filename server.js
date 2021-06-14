883e651b1ac859d4adc0148cea9e6778ea76d2dc
salammustafa728 <salammustafa728@gmail.com>
2021-06-14 20:09:34 +0300

    test

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const express = require('express') // require the express package
-const app = express() // initialize your express app instance
 const cors = require('cors');
+const weatherData = require('./data/weather.json')
+const app = express() // initialize your express app instance
+
 
 app.use(cors()) // after you initialize your express app instance
 // a server endpoint 
-require('dotenv').config();
+
 const port=process.env.port;
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
